db3b3fda763c36d25312f2bc956a0ffddea8868b
ASiracusa <fedorafilmz@gmail.com>
2019-11-25 17:50:32 -0500

    major updates to outtake
    might be close to completion or far from it, we'll decide when we test it tomorrow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/Outtake.java
@@ -1,72 +1,118 @@
 package org.firstinspires.ftc.teamcode.movement;
 
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Outtake {
+    ElapsedTime deltaTime;
+
     //Servo pickUpServo;
-    CRServo pickUpServo;
-    double pickUpTime;
+    CRServo claw;
+    double clawPos;
+    CRServo wrist;
+    double wristPos;
+    DcMotor arm;
 
-    void pickUp() {
-        //pickUpServo.setPosition(0.25);
+    public Outtake (DcMotor arm, CRServo wrist, CRServo claw) {
+        this.arm = arm;
+        this.wrist = wrist;
+        this.claw = claw;
+
+        clawPos = 0;
+        wristPos = 0;
     }
-    void dropServo() {
-        //pickUpServo.setPosition(0);
-    }
 
-    Motor arm;
-
-    public static class Motor {
-        String name = null;
-        DcMotor motor;
-
-        public Motor(DcMotor motor) {
-            this.motor = motor;
-        }
+    public void updateTime () {
+        deltaTime.reset();
     }
 
     // Moves the arm in the given direction.
     public void moveArm (boolean direction, boolean limited) {
-        int currpos = arm.motor.getCurrentPosition();
+        int currpos = arm.getCurrentPosition();
 
         // up = true, down = false
         if (direction && (!limited || currpos < 300)) {
-            arm.motor.setDirection(DcMotor.Direction.FORWARD);
+            arm.setDirection(DcMotor.Direction.FORWARD);
         } else if (!direction && (!limited || currpos > 0)) {
-            arm.motor.setDirection(DcMotor.Direction.REVERSE);
+            arm.setDirection(DcMotor.Direction.REVERSE);
         }
     }
 
     // Stops the arm's motor.
     public void stopArm () {
-        arm.motor.setPower(0);
+        arm.setPower(0);
     }
 
-    public void moveClawIn (double power) {
-        if (pickUpTime > 0) {
-            pickUpServo.setDirection(DcMotorSimple.Direction.REVERSE);
-        }
-    }
-
-    public void moveClawOut (double power) {
-        if (pickUpTime < 1) {
-            pickUpServo.setDirection(DcMotorSimple.Direction.FORWARD);
+    public void moveClaw (boolean clamping, double power) {
+        if (clamping && wristPos > 0) {
+            claw.setDirection(DcMotorSimple.Direction.FORWARD);
+            claw.setPower(power);
+        } else if (!clamping && wristPos < 1) {
+            claw.setDirection(DcMotorSimple.Direction.REVERSE);
+            claw.setPower(power);
+        } else {
+            claw.setPower(0);
         }
     }
 
     public void stopClaw () {
-        pickUpServo.setPower(0);
+        claw.setPower(0);
     }
 
-    public void updateClawTime (double time, boolean turningIn) {
-        if (turningIn) {
-            pickUpTime = pickUpTime - time;
-        } else {
-            pickUpTime = pickUpTime + time;
+    public void updateClawTime (boolean clamping) {
+        if (clamping && clawPos > 0) {
+            clawPos = clawPos - deltaTime.seconds();
+        } else if (!clamping && clawPos < 1){
+            clawPos = clawPos + deltaTime.seconds();
         }
+    }
+
+    public void rotateWrist (boolean left, double power) {
+        if (left && wristPos > -1) {
+            wrist.setDirection(DcMotorSimple.Direction.FORWARD);
+            wrist.setPower(power);
+        } else if (!left && wristPos < 1) {
+            wrist.setDirection(DcMotorSimple.Direction.REVERSE);
+            wrist.setPower(power);
+        } else {
+            wrist.setPower(0);
+        }
+    }
+
+    public void stopWrist () {
+        wrist.setPower(0);
+    }
+
+    public void updateWristTime (boolean left) {
+        if (left && clawPos > -1) {
+            clawPos = clawPos - deltaTime.seconds();
+        } else if (!left && clawPos < 1){
+            clawPos = clawPos + deltaTime.seconds();
+        }
+    }
+
+    private static String[] standardMotorNames = {"outtake_arm", "outtake_wrist", "outtake_claw"};
+
+    public static Outtake standard(HardwareMap hardwareMap) {
+
+        // arm
+        DcMotor arm = hardwareMap.get(DcMotor.class, standardMotorNames[0]);
+        arm.setDirection(DcMotor.Direction.FORWARD);
+
+        // wrist
+        CRServo wrist = hardwareMap.get(CRServo.class, standardMotorNames[1]);
+        wrist.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // claw
+        CRServo claw = hardwareMap.get(CRServo.class, standardMotorNames[2]);
+        claw.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        return new Outtake(arm, wrist, claw);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IterativeOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/IterativeOpMode.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.movement.Intake;
 import org.firstinspires.ftc.teamcode.movement.MecanumDrive;
 
+import org.firstinspires.ftc.teamcode.movement.Outtake;
 import org.firstinspires.ftc.teamcode.util.Utils;
 import org.firstinspires.ftc.teamcode.util.ButtonTracker;
 
@@ -13,12 +14,15 @@ import org.firstinspires.ftc.teamcode.util.ButtonTracker;
 public class IterativeOpMode extends OpMode {
     //private MecanumDrive drive;
     private Intake intake;
+    private Outtake outtake;
+
     private ButtonTracker flywheelBT;
 
     @Override
     public void init() {
         //drive = MecanumDrive.standard(hardwareMap);
         intake = Intake.standard(hardwareMap);
+        outtake = Outtake.standard(hardwareMap);
 
         flywheelBT = new ButtonTracker();
     }
@@ -30,9 +34,9 @@ public class IterativeOpMode extends OpMode {
         double strafe = gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
 
-            // Account for stick drifting
-            if (Utils.accountDrift(strafe, speed)) {
-                strafe = 0;
+        // Account for stick drifting
+        if (Utils.accountDrift(strafe, speed)) {
+            strafe = 0;
             speed = 0;
         } if (Utils.accountDrift(turn, 0)) {
             turn = 0;
@@ -41,8 +45,8 @@ public class IterativeOpMode extends OpMode {
         // Drive in the inputted direction.
         MecanumDrive.Motor.Vector2D vector = new MecanumDrive.Motor.Vector2D(strafe, speed);
         drive.move(1, vector, turn, MecanumDrive.TurnBehavior.ADDSUBTRACT);
-
          */
+
         // Activate and deactivate pivot flywheels (toggles)
         flywheelBT.ifPress(gamepad1.y);
 
@@ -55,6 +59,41 @@ public class IterativeOpMode extends OpMode {
 
         // Update BTs
         flywheelBT.update(gamepad1.y);
+
+        // Update outtake deltaTime
+        outtake.updateTime();
+
+        // Raise and lower arm
+        if (gamepad1.dpad_down) {
+            outtake.moveArm(false, true);
+        }
+        else if (gamepad1.dpad_up) {
+            outtake.moveArm(true, true);
+        } else {
+            outtake.stopArm();
+        }
+
+        // Rotate wrist
+        if (gamepad1.dpad_left) {
+            outtake.rotateWrist(true, 1);
+            outtake.updateWristTime(true);
+        } else if (gamepad1.dpad_right) {
+            outtake.rotateWrist(false, 1);
+            outtake.updateWristTime(false);
+        } else {
+            outtake.stopWrist();
+        }
+
+        // Clamp claw
+        if (gamepad1.left_bumper) {
+            outtake.moveClaw(true, 1);
+            outtake.updateClawTime(true);
+        } else if (gamepad1.right_bumper) {
+            outtake.moveClaw(false,1);
+            outtake.updateClawTime(false);
+        } else {
+            outtake.stopClaw();
+        }
 
         // Telemetry data
         /*
