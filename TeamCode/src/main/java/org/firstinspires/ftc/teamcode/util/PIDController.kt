a594ae74a602e05b0d991a0f97890aba255aa8a2
Anthony Li <li.anthony917@gmail.com>
2020-01-25 16:01:20 -0500

    Add PID controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PIDController.kt
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.util
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.teamcode.movement.MecanumDrive
+import org.firstinspires.ftc.teamcode.sensors.Gyro
+
+interface PIDControllerMovable {
+    val mecanumDrive: MecanumDrive
+    val gyro: Gyro
+
+    fun idle()
+}
+
+class PIDController(
+        var Kp: Double,
+        var Ki: Double,
+        var Kd: Double
+) {
+    private var integral: Double = 0.0
+    private var previous: Double = 0.0
+
+    fun reset() {
+        integral = 0.0
+        previous = 0.0
+    }
+
+    init {
+        reset()
+    }
+
+    fun evaluate(current: Double, setpoint: Double, deltaTime: Double): Double {
+        val error = setpoint - current
+        integral += error
+
+        val result = Kp * error + Ki * integral * deltaTime + Kd * (error - previous) / deltaTime
+        previous = error
+
+        return result
+    }
+
+    fun move(opMode: PIDControllerMovable, power: Double, vector: MecanumDrive.Motor.Vector2D, time: Long) {
+        val elapsedTime = ElapsedTime()
+        val startTime = elapsedTime.milliseconds()
+        val setpoint = opMode.gyro.angle
+        var previous = startTime
+
+        while (elapsedTime.milliseconds() - startTime < time) {
+            val now = elapsedTime.milliseconds()
+
+            opMode.mecanumDrive.move(power, vector, evaluate(opMode.gyro.angle, setpoint, now - previous))
+            opMode.idle()
+
+            previous = now
+        }
+
+        opMode.mecanumDrive.stop()
+    }
+
+    fun strafeLeft(opMode: PIDControllerMovable, power: Double, time: Long) =
+            move(opMode, power, MecanumDrive.Motor.Vector2D(-1.0, 0.0), time)
+
+    fun forward(opMode: PIDControllerMovable, power: Double, time: Long) =
+            move(opMode, power, MecanumDrive.Motor.Vector2D(0.0, 1.0), time)
+}
