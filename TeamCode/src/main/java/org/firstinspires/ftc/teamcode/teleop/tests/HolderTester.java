381fb846a085c6c6caebab2a575857cd99366889
Anthony Li <antli22@bergen.org>
2020-01-26 10:03:57 -0500

    Make HolderTesters work with new BrickHolder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tests/HolderTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tests/HolderTester.java
@@ -42,7 +42,7 @@ public class HolderTester extends OpMode {
 
         telemetry.addData("Arm A Pos: ", holder.armA.getPosition());
         telemetry.addData("Arm B Pos: ", holder.armB.getPosition());
-        telemetry.addData("Claw Pos: ", holder.claw.getPosition());
+        telemetry.addData("Claw Pos: ", holder.clawA.getPosition());
         telemetry.addData("Speed", speed);
     }
 
